4d62fde749487ac5c235b17aa220170c4328853c
Shamil Mukhaev <144003829+YopsTudei@users.noreply.github.com>
2024-12-25 10:32:53 +0700

    Update t10.C

--- t10.C
+++ t10.C
@@ -38,21 +38,21 @@ void combinedChi2(int& npar, double* grad, double& f, double* par, int flag) {
         double observed = gHist1->GetBinContent(i);
         double expected = c + A * std::exp(-0.5 * std::pow((x - mu) / sigma, 2.0));  
         double error = gHist1->GetBinError(i);
-        chi2_1 += ((observed - expected) / error)*((observed - expected) / error);
+        chi2_1 += ((observed - expected) / error) * ((observed - expected) / error);
     }
-    
     double chi2_2 = 0.0;
     for (int i = 1; i <= gHist2->GetNbinsX(); ++i) {
         double observed = gHist2->GetBinContent(i);
         double error = gHist2->GetBinError(i);
         double expected = c; 
-        chi2_2 += ((observed - expected) / error)*((observed - expected) / error);
+        chi2_2 += ((observed - expected) / error) * ((observed - expected) / error);
     }
-    
     f = chi2_1 + chi2_2;
 }
 
 void fitHistogramsSimultaneously(TH1F* hist1, TH1F* hist2) {
+    gHist1 = hist1;
+    gHist2 = hist2;
     TMinuit minimizer(4);
     minimizer.SetFCN(combinedChi2);
     minimizer.DefineParameter(0, "c", 1.0, 0.1, 0.0, 10.0);
@@ -61,28 +61,46 @@ void fitHistogramsSimultaneously(TH1F* hist1, TH1F* hist2) {
     minimizer.DefineParameter(3, "sigma", 10, 0.1, 1.0, 50.0);
     minimizer.Migrad();
     
-    double currentValue, currentError;
+    double c, A, mu, sigma, c_err, A_err, mu_err, sigma_err;
+    minimizer.GetParameter(0, c, c_err);
+    minimizer.GetParameter(1, A, A_err);
+    minimizer.GetParameter(2, mu, mu_err);
+    minimizer.GetParameter(3, sigma, sigma_err);
     std::cout << "Fit results:" << std::endl;
-    for (int i = 0; i < 4; ++i) {
-        minimizer.GetParameter(i, currentValue, currentError);
-        std::cout << "Parameter " << i << " = " << currentValue << " ± " << currentError << std::endl;
-    }
+    std::cout << "c = " << c << " ± " << c_err << std::endl;
+    std::cout << "A = " << A << " ± " << A_err << std::endl;
+    std::cout << "mu = " << mu << " ± " << mu_err << std::endl;
+    std::cout << "sigma = " << sigma << " ± " << sigma_err << std::endl;
+
+    double Nsignal = A * sigma * sqrt(2 * M_PI);
+    std::cout << "Nsignal (number of events under Gaussian) = " << Nsignal << std::endl;
+
+    TF1* func1 = new TF1("func1", "[0] + [1] * exp(-0.5 * pow((x - [2]) / [3], 2.0))", 500, 600);
+    func1->SetParameters(c, A, mu, sigma);
+    hist1->Fit(func1, "R");
+
+    TF1* func2 = new TF1("func2", "[0]", 500, 600);
+    func2->SetParameter(0, c);
+    hist2->Fit(func2, "R");
 }
 
-void analyzeData() {
+
+void t10() {
     std::vector<double> data1, data2;
     loadData("data_1.dat", data1);
     loadData("data_2.dat", data2);
     TH1F* hist1 = createHistogram(data1, "hist1");
     TH1F* hist2 = createHistogram(data2, "hist2");
-    gHist1 = hist1;
-    gHist2 = hist2;
     fitHistogramsSimultaneously(hist1, hist2);
     TCanvas* canvas1 = new TCanvas("canvas1", "Histogram 1", 800, 600);
+    hist1->GetXaxis()->SetTitle("Units");
+    hist1->GetYaxis()->SetTitle("Counts");
     hist1->Draw();
-    TCanvas* canvas2 = new TCanvas("canvas2", "Histogram 2", 800, 600);
-    hist2->Draw();
     canvas1->SaveAs("hist1_fit.pdf");
+    TCanvas* canvas2 = new TCanvas("canvas2", "Histogram 2", 800, 600);
+    hist2->GetXaxis()->SetTitle("Units");
+    hist2->GetYaxis()->SetTitle("Counts");
+    hist2->Draw();
     canvas2->SaveAs("hist2_fit.pdf");
     delete canvas1;
     delete canvas2;
